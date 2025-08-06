9fd3c29b9c6a8e6729d439aa4e9d660df33c371b
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2025-08-06 18:41:25 +0200

    Refactor startLabel to improve readability and maintainability

--- assets/manifest.ts
+++ assets/manifest.ts
@@ -5,6 +5,24 @@ import { AssetsManifest } from "@drincs/pixi-vn";
  * You can read more about the manifest here: https://pixijs.com/8.x/guides/components/assets#loading-multiple-assets
  */
 const manifest: AssetsManifest = {
-  bundles: [],
+  bundles: [
+    {
+      name: "start",
+      assets: [
+        {
+          alias: "m01-body",
+          src: "https://firebasestorage.googleapis.com/v0/b/pixi-vn.appspot.com/o/public%2Fbreakdown%2Fm01%2Fm01-body.webp?alt=media",
+        },
+        {
+          alias: "m01-eyes",
+          src: "https://firebasestorage.googleapis.com/v0/b/pixi-vn.appspot.com/o/public%2Fbreakdown%2Fm01%2Fm01-eyes-smile.webp?alt=media",
+        },
+        {
+          alias: "m01-mouth",
+          src: "https://firebasestorage.googleapis.com/v0/b/pixi-vn.appspot.com/o/public%2Fbreakdown%2Fm01%2Fm01-mouth-smile00.webp?alt=media",
+        },
+      ],
+    },
+  ],
 };
 export default manifest;
--- labels/startLabel.ts
+++ labels/startLabel.ts
@@ -1,6 +1,14 @@
-import { narration, newLabel } from "@drincs/pixi-vn";
+import { canvas, MoveTicker, newLabel, showImageContainer } from "@drincs/pixi-vn";
 
 export const startLabel = newLabel("start_label", [
-  () => (narration.dialogue = "Hello"),
-  (props, { labelId }) => narration.jumpLabel(labelId, props),
+  async () => {
+    let james = await showImageContainer("james", ["m01-body", "m01-eyes", "m01-mouth"], {
+      xAlign: 0.5,
+      yAlign: 1,
+    });
+  },
+  () => {
+    canvas.removeAllTickers();
+    let tickerId = canvas.addTicker("james", new MoveTicker({ destination: { x: 0, y: 1, type: "align" } }));
+  },
 ]);
