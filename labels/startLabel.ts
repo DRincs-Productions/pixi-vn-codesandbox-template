85eccb1e0154ea720fadd721a2d23c8ca1e0009c
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2025-08-15 17:48:43 +0200

    Refactor startLabel to remove unused functionality and simplify initialization

--- labels/startLabel.ts
+++ labels/startLabel.ts
@@ -1,14 +1,3 @@
-import { canvas, MoveTicker, newLabel, showImageContainer } from "@drincs/pixi-vn";
+import { newLabel } from "@drincs/pixi-vn";
 
-export const startLabel = newLabel("start_label", [
-  async () => {
-    let james = await showImageContainer("james", ["m01-body", "m01-eyes", "m01-mouth"], {
-      xAlign: 0.5,
-      yAlign: 1,
-    });
-  },
-  () => {
-    canvas.removeAllTickers();
-    let tickerId = canvas.addTicker("james", new MoveTicker({ destination: { x: 0, y: 1, type: "align" } }));
-  },
-]);
+export const startLabel = newLabel("start_label", [() => {}]);
